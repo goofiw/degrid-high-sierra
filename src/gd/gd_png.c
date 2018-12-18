adb22e78238fab2e9f8d4f72222f248d755ae527
goofiw <goofiw@gmail.com>
2018-12-18 15:17:07 -0800

    modified file in gd and macos script to use tlc-tk from brew

--- src/gd/gd_png.c
+++ src/gd/gd_png.c
@@ -143,7 +143,7 @@ BGD_DECLARE(gdImagePtr) gdImageCreateFromPngCtx (gdIOCtx * infile)
   /* first do a quick check that the file really is a PNG image; could
    * have used slightly more general png_sig_cmp() function instead */
   gdGetBuf (sig, 8, infile);
-  if (!png_check_sig (sig, 8))
+  if (png_sig_cmp (sig, 0, 8))
     return NULL;		/* bad signature */
 
 #ifndef PNG_SETJMP_NOT_SUPPORTED
